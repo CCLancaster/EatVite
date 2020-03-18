14424689f349b5d776a2c0fc3f1272d374a6ae58
tinnerrss <tinnerrss@hotmail.com>
2020-03-18 15:25:26 -0700

    changed file name

---
+++ client/src/content/pages/Profile.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Profile(props) {
+    return (
+        <div className="profile">
+            <div className='friendlist'>
+                <div>
+                    
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+
+
+            </div>
+            <div className='events'>
+                <div>
+                    
+                </div>
+                <div>
+    
+                </div>
+                <div>
+    
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Profile;
